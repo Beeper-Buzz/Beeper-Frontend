fe17e620b806d7f8555fab557ae317244a5349b4
Aaron Smulktis <aaronsmulktis@gmail.com>
2023-08-04 18:55:56 -0400

    fixed styling of input and buttons

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,1 +1,36 @@
+<<<<<<< HEAD
 export { Layout } from "./Layout";
+=======
+import React from "react";
+
+export const Layout = ({ children }) => {
+  return (
+    <main>
+      {children}
+      <style jsx global>{`
+        * {
+          font-family: IBM Plex Mono, Menlo, Monaco, "Lucida Console", "Liberation Mono",
+            "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Courier New",
+            monospace, serif;
+        }
+        body {
+          margin: 0;
+          padding: 25px 50px;
+        }
+        a {
+          color: #22bad9;
+        }
+        p {
+          font-size: 14px;
+          line-height: 24px;
+        }
+        article {
+          margin: 0 auto;
+          max-width: 650px;
+        }
+
+      `}</style>
+    </main>
+  );
+};
+>>>>>>> 4f8e275 (fixed styling of input and buttons)
