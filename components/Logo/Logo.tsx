546b88008504f45666ff1c2f4867a62c472d37fe
Aaron Smulktis <aaronsmulktis@gmail.com>
2023-08-04 20:56:10 -0400

    update social links

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import {
-  LogoMark
-} from './Logo.styles';
+import { LogoMark } from "./Logo.styles";
 
 export const Logo = ({ hasBlob, isDark }: any) => {
-
   return (
     <>
       <LogoMark src={process.env.NEXT_PUBLIC_LOGO_PATH} />
     </>
-  )
+  );
 };
