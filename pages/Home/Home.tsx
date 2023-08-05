ed6449dd1235d3f71a8fc69b1db4789eb33d5b54
Aaron Smulktis <aaronsmulktis@gmail.com>
2023-08-04 20:10:38 -0400

    move home page to directory, add root index files to added named file for Home.tsx

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,32 +1,22 @@
 import React from "react";
 import { QueryClient } from "react-query";
 import { dehydrate } from "react-query/hydration";
-import { ComingSoon, Layout, InfoBox, ProductList } from "../../components";
+import { Layout, ProductList } from "../../components";
 import { fetchPosts, fetchProducts } from "../../hooks";
-
-const renderHomeContent = () => {
-  if (process.env.IS_MAINT_MODE == "true") {
-    console.log();
-    return <ComingSoon />;
-  }
-
-  return <ProductList />;
-};
 
 const Home = () => {
   return (
     <Layout>
-      {renderHomeContent()}
-      <div className={"test"}></div>
+      <ProductList />
     </Layout>
   );
 };
 
 export async function getServerSideProps() {
-    const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-    await queryClient.prefetchQuery(['posts', 10], () => fetchPosts(10));
-    await queryClient.prefetchQuery(['products', 1], () => fetchProducts(1));
+  await queryClient.prefetchQuery(["posts", 10], () => fetchPosts(10));
+  await queryClient.prefetchQuery(["products", 1], () => fetchProducts(1));
 
   return {
     props: {
